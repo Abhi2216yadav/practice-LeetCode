12ca3b7f7558b16aef7a80a04805d2f128d0eee2
Abhay Yadav <abhay189992@gmail.com>
2026-09-21 01:05:34 +0530

    Time: 1 ms (42.67%), Space: 10.7 MB (7.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    bool isAlphanum(char ch){
-        if((ch >= '0' && ch<= '9')|| (tolower(ch)>='a' &&  tolower(ch) <= 'z')){
+    bool isPalindrome(string s) {
+        if(s.length() == 0){
             return true;
         }
-        return false;
-    }
-    bool isPalindrome(string s) {
-        int st =0 ,end = s.length()-1;
-        while(st < end){
-            if(!isAlphanum(s[st])){
-                st++;continue;
+
+        string str = "";
+        for(char c : s){
+            if (isalnum(c)) {
+                str += tolower(c);
             }
-            if(!isAlphanum(s[end])){
-                end--;continue;
-            }
-            if(tolower(s[st]) != tolower(s[end])){
-                return false;
-            }
-            st++;end--;
         }
-        return true;
+
+        string str2 = str;
+        reverse(str2.begin(), str2.end());
+
+        return str == str2;
     }
 };
